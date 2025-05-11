b8184ded03a70f6ae55afcc7e0d56d903383a3e9
Haqqnuru <haqqnuru@gmail.com>
2025-05-11 20:09:02 +0000

    adding sagas to cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,14 +1,14 @@
 
-import { CartActionTypes } from './cart.types';
+import CartActionTypes from './cart.types';
 
 //action for cart to appear or stay hidden
 export const toggleCartHidden = () => ({
-  type:CartActionTypes.TOGGLE_CART_HIDDEN
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
 // action for the cart to add
 export const addItem = item => ({
-  type:CartActionTypes.ADD_ITEM,
+  type: CartActionTypes.ADD_ITEM,
   payload: item
 });
 
@@ -20,6 +20,11 @@ export const removeItem = item => ({
 
 // action for the cart to clear
 export const clearItemFromCart = item => ({
-  type:CartActionTypes.CLEAR_ITEM_FROM_CART,
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
   payload: item
+});
+
+
+export const clearCart = () => ({
+  type: CartActionTypes.CLEAR_CART
 });
